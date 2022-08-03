1fb702ed0f1c8e8bca5a48992d5d657aefa587e8
derpflections <leehongyi91@gmail.com>
2022-08-03 15:02:01 +0800

    working redirect w/ loading

--- javascript/index/indexScrollRedirect.js
+++
@@ -1,9 +0,0 @@
-function nextpage1(){
-    console.log(document.documentElement.scrollTop)
-    console.log(document.getElementById("nextpage").scrollTop)
-    if (document.getElementById("nextpage").scrollTop >= 92){
-        window.location.replace("experience.html")
-    }
-}
-currentPage = (window.location.pathname)
-//setInterval(nextpage, 50)
--- javascript/loading/loadingRedirect.js
+++ javascript/loading/loadingRedirect.js
@@ -1,1 +1,23 @@
-function pageRedirect
+var initialPage = ""
+var destPage = ""
+var finalPage = ""
+var url = window.location.pathname;
+var filename = url.substring(url.lastIndexOf('/')+1);
+
+//document.getElementById("nextpage").addEventListener("scroll", indexPageRedirect)
+
+function indexPageRedirect(){
+    initialPage = "index.html"
+    destPage = "experience.html"
+    sessionStorage.setItem("initialPage", initialPage)
+    sessionStorage.setItem("destPage", destPage)
+    window.location.assign("loadingPage.html")
+}
+
+function destPageRedirect(){
+    window.location.assign(sessionStorage.getItem("destPage"))
+}
+
+if (filename == 'loadingPage.html'){
+    setTimeout(destPageRedirect, 2500)
+}
