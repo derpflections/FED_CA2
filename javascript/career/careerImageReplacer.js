a2ccaf32b8e593b2cc542a75ff61863b04cbf25c
derpflections <leehongyi91@gmail.com>
2022-08-06 14:46:12 +0800

    aug 6

--- javascript/career/careerImageReplacer.js
+++ javascript/career/careerImageReplacer.js
@@ -1,5 +1,6 @@
-"use strict"
 var windowHeight = 0 , windowWidth = 0
+document.getElementById("c1pic").style.width = 420
+document.getElementById("c1pic").style.height = 320
 
 function pollViewport (){
     windowHeight = window.innerHeight
@@ -14,5 +15,26 @@ function imageReplace (){
     }
 }
 
+function imageScale(){
+    if(window.innerWidth > 1600){
+        document.getElementById("c1pic").width = (window.innerWidth * 0.21875)
+        document.getElementById("c1pic").height = (window.innerWidth * 0.21875) / 1.3125
+        document.getElementById("c2pic").width = (window.innerWidth * 0.21875)
+        document.getElementById("c2pic").height = (window.innerWidth * 0.21875) / (460/360)
+        document.getElementById("c3pic").width = (window.innerWidth * 0.21875)
+        document.getElementById("c3pic").height = (window.innerWidth * 0.21875) / (460/300)
+    } else if (window.innerWidth < 1200){
+        document.getElementById("c1pic").width = 0.8 * window.innerWidth
+        document.getElementById("c1pic").height = (0.8 * window.innerWidth)/ 1.3125
+        document.getElementById("c2pic").width = 0.8 * window.innerWidth
+        document.getElementById("c2pic").height = (0.8 * window.innerWidth)/ (460/360)
+        document.getElementById("c3pic").width = 0.8 * window.innerWidth
+        document.getElementById("c3pic").height = (0.8 * window.innerWidth)/ (460/300)
+    }
+}
+
+
+
 setInterval(pollViewport, 50)
 setInterval(imageReplace, 50)
+setInterval(imageScale, 50)
